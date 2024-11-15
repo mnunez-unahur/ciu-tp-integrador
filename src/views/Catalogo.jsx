734bdf9e654d2cff82d2166a11be63b85b551955
112630074+mnunez-unahur <112630074+mnunez-unahur@users.noreply.github.com>
2024-11-15 00:31:39 +0000

    implementacion básica de vista de productos

--- src/views/Catalogo.jsx
+++ src/views/Catalogo.jsx
@@ -1,7 +1,52 @@
+import React, { useState, useEffect } from 'react';
+
+import SearchBar from '../components/SearchBar'
+import ProductCard from '../components/ProductCard'
+
+// importamos los modelos
+import getProductos from '../model/productos';
+
 function Catalogo() {
+    const [busqueda, setBusqueda] = useState("cosito del coso");
+    const [listaProductos, setListaProductos] = useState([]);
+    const [productosFiltrados, setProductosFiltrados] = useState([]);
+
+
+    const filtrar = (texto) => {
+        setBusqueda(texto)
+        texto = texto.toLowerCase()
+        const pf = listaProductos.filter((p)=>p.nombre.toLowerCase().includes(texto) || p.descripcion.toLowerCase().includes(texto))
+        setProductosFiltrados(pf)
+      };
+
+    useEffect(() => {
+        console.log("limpiando")
+        filtrar("")
+    }, [listaProductos]);
+
+
+    useEffect(() => {
+        async function getP() {
+            const prods = await getProductos()
+            console.log(prods)
+            setListaProductos(prods)
+        }
+        getP()
+    }, []);
+
+
     return (
+
     <div>
-     Catalogo
+        <SearchBar value={busqueda} onChange={filtrar} />
+        <ul>
+        {productosFiltrados.map(producto => (
+            <li>
+            <span>{producto.nombre}</span>
+            <span>{producto.descripcion}</span>
+            </li>
+        ))}            
+        </ul>
     </div>
     );
 }
