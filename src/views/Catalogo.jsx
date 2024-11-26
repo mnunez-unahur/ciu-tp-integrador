458ed5ca3c7997db11ab6482bb82c30ac04ce994
112630074+mnunez-unahur <112630074+mnunez-unahur@users.noreply.github.com>
2024-11-26 00:47:14 +0000

    se implementa carro de compra

--- src/views/Catalogo.jsx
+++ src/views/Catalogo.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
-import {Row, Col, Breadcrumb, Button, Badge, Container} from 'react-bootstrap';
+import { Row, Col, Breadcrumb, Button, Badge, Container } from 'react-bootstrap';
 
 import SearchBar from '../components/SearchBar'
 import ProductCard from '../components/ProductCard'
 import DetalleProducto from '../components/DetalleProducto'
+import DetalleCarrito from '../components/DetalleCarrito'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
@@ -15,92 +16,118 @@ import { getProductos } from '../services/productos';
 
 
 function Catalogo() {
-    const [busqueda, setBusqueda] = useState("cosito del coso");
-    const [listaProductos, setListaProductos] = useState([]);
-    const [productosFiltrados, setProductosFiltrados] = useState([]);
-    const [carrito, setCarrito] = useState([])
-    const [propiedadesVisibles, setPropiedadesVisibles] = React.useState(false);
-    const [productoActual, setProductoActual] = React.useState(0);
+  const [busqueda, setBusqueda] = useState("cosito del coso");
+  const [listaProductos, setListaProductos] = useState([]);
+  const [productosFiltrados, setProductosFiltrados] = useState([]);
+  const [carrito, setCarrito] = useState([])
+  const [mostrarDetalles, setMostrarDetalles] = React.useState(false);
+  const [productoActual, setProductoActual] = React.useState(0);
+  const [mostrarCarrito, setMostrarCarrito] = React.useState(false);
 
 
-    const filtrar = (texto) => {
-        setBusqueda(texto)
-        texto = texto.toLowerCase()
-        console.log(listaProductos)
-        const pf = listaProductos.filter((p) => p.nombre.toLowerCase().includes(texto) || p.descripcion.toLowerCase().includes(texto))
-        setProductosFiltrados(pf)
-    };
+  const filtrar = (texto) => {
+    setBusqueda(texto)
+    texto = texto.toLowerCase()
+    console.log(listaProductos)
+    const pf = listaProductos.filter((p) => p.nombre.toLowerCase().includes(texto) || p.descripcion.toLowerCase().includes(texto))
+    setProductosFiltrados(pf)
+  };
 
-    useEffect(() => {
-        filtrar("")
-    }, [listaProductos]);
+  useEffect(() => {
+    filtrar("")
+  }, [listaProductos]);
 
 
-    async function getP() {
-        const prods = await getProductos()
-        setListaProductos(prods)
+  async function getP() {
+    const prods = await getProductos()
+    setListaProductos(prods)
+  }
+
+  useEffect(() => {
+    getP()
+  }, []);
+
+  function editarProducto(id) {
+    setProductoActual(id)
+    setMostrarDetalles(true)
+  }
+
+  function cerrarDialogoProducto() {
+    setProductoActual(0)
+    setMostrarDetalles(false)
+  }
+
+  function agregarQuitarCarrito(id, agregar) {
+    let prod = listaProductos.find(p => p.id == id)
+    if (!prod) {
+      console.error(`no se encontro el producto ${id}`)
+      return
     }
+    prod = {...prod}
 
-    useEffect(() => {
-        getP()
-    }, []);
-
-    function editarProducto(id) {
-      setProductoActual(id)
-      setPropiedadesVisibles(true)
+    if (agregar) {
+      prod.cantidad = 1
+      const productos = [...carrito]
+      productos.push(prod)
+      setCarrito(productos)
+    } else {
+      const productos = carrito.filter(p => {
+        return p.id != id
+      })
+      setCarrito(productos)
     }
+  }
 
-    function cerrarDialogoProducto() {
-      setProductoActual(0)
-      setPropiedadesVisibles(false)
-    }
+  function estaEnCarrito(id) {
+    const idx = carrito.findIndex(c=> c.id == id)
+    return idx >= 0
+  }
 
-    function agregarQuitarCarrito(id, estado) {
+  return (
 
-        if (estado) {
-            const productos = [...carrito]
-            productos.push(id)
-            setCarrito(productos)
-        } else {
-            const productos = carrito.filter(p => {
-                return p != id
-            })
-            setCarrito(productos)
-        }
-    }
+    <Container>
+      <Breadcrumb>
+        <Breadcrumb.Item active>Inicio</Breadcrumb.Item>
+        <Breadcrumb.Item active>Catalogo</Breadcrumb.Item>
+      </Breadcrumb>
+      <Button
+        onClick={()=>{setMostrarCarrito(true)}}
+        style={{ position: "fixed", top: "50px", right: "50px", zIndex: "1000" }}>
+        <FontAwesomeIcon icon={faCartShopping} />
+        <Badge bg="secondary">  {carrito.length}</Badge>
+      </Button>
+      <SearchBar value={busqueda} onChange={filtrar} placeholder='Buscar producto...' />
+      {productosFiltrados.map(p => (
+        <Row key={p.id}>
+          <Col style={{ padding: "10px" }}>
+            <ProductCard
+              id={p.id}
+              nombre={p.nombre}
+              descripcion={p.descripcion}
+              precio={p.precio}
+              img={p.pathImg}
+              onChangeCarrito={agregarQuitarCarrito}
+              enCarrito={estaEnCarrito(p.id) }
+              onEdit={editarProducto}
+            />
 
-    return (
+          </Col>
+        </Row>
+      ))}
+      <DetalleProducto
+        idProducto={productoActual}
+        show={mostrarDetalles}
+        onHide={cerrarDialogoProducto}
+      />
 
-        <Container>
-            <Breadcrumb>
-                <Breadcrumb.Item active>Inicio</Breadcrumb.Item>
-                <Breadcrumb.Item active>Catalogo</Breadcrumb.Item>
-            </Breadcrumb>
-            <Button style={{position:"fixed", top:"50px",right:"50px", zIndex: "1000"}}><FontAwesomeIcon icon={faCartShopping} /> <Badge bg="secondary">  {carrito.length} </Badge></Button>
-            <SearchBar value={busqueda} onChange={filtrar} placeholder='Buscar producto...' />
-            {productosFiltrados.map(p => (
-                <Row key={p.id}>
-                    <Col style={{ padding: "10px" }}>
-                        <ProductCard
-                            id={p.id}
-                            nombre={p.nombre}
-                            descripcion={p.descripcion}
-                            precio={p.precio}
-                            img={p.pathImg}
-                            onChangeCarrito={agregarQuitarCarrito}
-                            enCarrito={carrito.includes(p.id)}
-                            onEdit={editarProducto}
-                        />
+      <DetalleCarrito
+        carrito={carrito} onChange={c => {setCarrito(c)}}
+        show={mostrarCarrito}
+        onHide={() => { setMostrarCarrito(false) }}
 
-                    </Col>
-                </Row>
-            ))}
-        <DetalleProducto
-          idProducto={productoActual}
-          show={propiedadesVisibles}
-          onHide={cerrarDialogoProducto}
-        />
-        </Container>
-    );
+      />
+
+    </Container>
+  );
 }
 export default Catalogo;
